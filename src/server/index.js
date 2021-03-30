c98589e73b99ad46e2d574084f8afcc0914408a2
eva.wang <eva.wang81@gmail.com>
2021-03-30 11:39:25 +0800

    Basic config

--- src/server/index.js
+++ src/server/index.js
@@ -1,21 +1,35 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+if (process.env.Node_ENV !== "production") {
+    require('dotenv').config();
+}
+const apiKey = process.env.API_KEY;
 
+
+const path = require('path');
+const express = require('express');
 const app = express()
+const cors = require('cors');
+const { request } = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-app.use(express.static('dist'))
+console.log(apiKey);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(express.static('dist'));
+
+// designates what port the app will listen to for incoming requests
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+});
 
 console.log(__dirname)
 
+
+
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    res.sendFile('dist/index.html')
 })
 
 app.get('/test', function (req, res) {
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,19 +1,30 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    devServer: {
+        port: 8080
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
         ]
     },
