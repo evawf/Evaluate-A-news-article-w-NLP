5b4fa66d48ee3aefc58fa9866b40c00a59e708f7
eva.wang <eva.wang81@gmail.com>
2021-04-01 12:54:27 +0800

    Setup server side code

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,57 @@
 function handleSubmit(event) {
     event.preventDefault()
+    
+    const postData = async (url = '', data = {}) => {
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        });
+        try {
+            const newData = await response.json();
+            return newData;
+        } catch(error) {
+            console.log('error', error);
+        }
+    }
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    const formInput = document.getElementById('url');
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    });
+
+    if(Client.checkForUrl(formInput)){
+        getAnalysis;
+    }else{
+        console.log("ERROR!")
+    }
+
+    const getAnalysis = async () => {
+        let data = await postData('/analyse', {
+            formInput: formInput.value
+        });
+        displayResults();
+        console.log(data);
+    }
+
+    const displayResults = async () => {
+        const request = await fetch('/analyse');
+        try{
+            const allData = await request.json();
+            console.log(allData);
+            const model = document.getElementById('model');
+            const score_tag = document.getElementById('score_tag');
+            const agreement = document.getElementById('agreement');
+            const subjectivity = document.getElementById('subjectivity');
+            const confidence = document.getElementById('confidence');
+            const irony = document.getElementById('irony');
+        } catch(error) {
+            console.log('error', error);
+        }
+    }
+
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -3,15 +3,15 @@ if (process.env.Node_ENV !== "production") {
 }
 const apiKey = process.env.API_KEY;
 
-
 const path = require('path');
 const express = require('express');
 const app = express()
 const cors = require('cors');
-const { request } = require('express');
+// const { request, response } = require('express');
 const mockAPIResponse = require('./mockAPI.js');
-
-console.log(apiKey);
+const FormData = require('form-data');
+const fetch = require("node-fetch");
+const { response } = require('express');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,13 +19,11 @@ app.use(cors());
 app.use(express.static('dist'));
 
 // designates what port the app will listen to for incoming requests
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 });
 
 console.log(__dirname)
-
-
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -35,3 +33,37 @@ app.get('/', function (req, res) {
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+let data = [];
+app.post('/analyse', getAnalysis);
+async function getAnalysis(req, res){
+    let formInput = req.body.formInpput;
+    // let formInput = "https://www.channelnewsasia.com/news/asia/japan-cherry-blossom-sakura-season-peak-bloom-climate-change-14528756";
+    let results = await getAnalysisData(formInput);
+    let showData = {
+        model: results.model,
+        score_tag: results.score_tag,
+        agreement: results.agreement,
+        subjectivity: results.subjectivity,
+        confidence: results.confidence,
+        irony: results.irony
+    }
+    data.push(showData);
+    res.send(data);
+}
+
+//Fetch API data
+const getAnalysisData = async (formInput) => {
+    const baseURL = "http://api.meaningcloud.com";
+    const subURL = "/sentiment-2.1?key=" + apiKey + "&lang=auto&url=" + formInput;
+    const url = baseURL + subURL;
+    console.log(url);
+    const res = await fetch(url) 
+    try{
+        const Data = await res.json();
+        return Data;
+    }
+    catch(error){
+        console.log('error', error);
+    }
+}
